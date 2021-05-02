e442dca37d52eab6dbc7ff8cb0268a837dd54415
albin ansel <albinaj98@gmail.com>
2021-05-03 00:55:08 +0530

    bank application

---
+++ javascript/mybankApp/bankscript.js
@@ -0,0 +1,26 @@
+class Bank{
+
+    static getAccountDetails(){
+    var users={
+    1000:{acno:1000,actype:"savings",username:"userone",password:"userone",balance:50000},
+    1001:{acno:1001,actype:"savings",username:"usertwo",password:"usertwo",balance:5000},
+    1002:{acno:1002,actype:"current",username:"userthree",password:"userthree",balance:10000},
+    1003:{acno:1003,actype:"current",username:"userfour",password:"userfour",balance:6000}
+    }
+ return users
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
