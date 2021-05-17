dadf7cef3b97e4269e814c0a004d3d56363cf66b
albin ansel <albinaj98@gmail.com>
2021-05-17 18:14:34 +0530

    Data binding - component to view and view to component(event binding)

---
+++ ANGULAR/Bank-App/src/app/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  aim = "Your perfect banking partner";
+
+  accno = "Account number please";
+  pswd = "";
+
+  accountDetails: any = {
+    1000: { acno: 1000, username: "userone", password: "userone", balance: 50000 },
+    1001: { acno: 1001, username: "usertwo", password: "usertwo", balance: 5000 },
+    1002: { acno: 1002, username: "userthree", password: "userthree", balance: 10000 },
+    1003: { acno: 1003, username: "userfour", password: "userfour", balance: 6000 }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  accnoChange(event:any){
+    this.accno=event.target.value;
+    console.log(this.accno);
+  }
+
+  pswdChange(event:any){
+    this.pswd=event.target.value;
+    console.log(this.pswd);
+  }
+
+
+  login() {
+    //  alert("Login Clicked");
+
+    var acno = this.accno;
+    var pswd = this.pswd;
+    let users = this.accountDetails;
+
+    if (acno in users) {
+      if (pswd == users[acno]["password"]) {
+        alert("Login Successful")
+      }
+      else {
+        alert("Incorrect Pasword")
+      }
+
+    }
+    else {
+      alert("invalid Account")
+    }
+  }
+
+}
